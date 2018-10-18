0ec8cb5b19faae2647d72114854dbf4c093d9008
ANKIT <kalsariyaankit1@gmail.com>
2018-10-18 16:59:44 +0530

    Update

--- app/src/main/java/com/rencorp/fohire/HomeActivity.java
+++ app/src/main/java/com/rencorp/fohire/HomeActivity.java
@@ -4,11 +4,15 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class HomeActivity extends AppCompatActivity {
+import com.miguelcatalan.materialsearchview.MaterialSearchView;
 
+public class HomeActivity extends AppCompatActivity {
+    MaterialSearchView searchView;
     //private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
@@ -38,9 +42,63 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //getSupportActionBar().hide();
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+
+
+        searchViewCode();
+
         //mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+    private void searchViewCode() {
+        searchView = (MaterialSearchView) findViewById(R.id.search_View);
+        searchView.setSuggestions(getResources().getStringArray(R.array.search));
+        searchView.setEllipsize(true);
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.searchmenu,menu);
+        MenuItem item = menu.findItem(R.id.action_search);
+        searchView.setMenuItem(item);
+
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_search :
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+    }
+    @Override
+    public void onBackPressed() {
+
+        if(searchView.isSearchOpen()) {
+            searchView.closeSearch();
+        }
+        else {
+            super.onBackPressed();
+        }
     }
 
 }
